a309aa89852ded9aa3ffd391d29575e6994ac086
CristianBatz <ce.sbatz@udeo.edu.gt>
2025-07-16 11:34:57 -0600

    correccion de errores y de texto

--- RegistroEstudiantes.py
+++ RegistroEstudiantes.py
@@ -12,7 +12,7 @@ while opcion != 4:
     if opcion == 1:
         print("=== Registro ===")
         cantidad = int(input("Cantidad de estudiantes que desea registrar: "))
-        while cantidad <= 0 or cantidad >=100:
+        while cantidad <= 0 or cantidad >= 100:
             print("Ha ingresado un numero fuera de los limites,vuelva a intentarlo")
             cantidad = int(input("Cantidad de estudiantes que desea registrar: "))
         for i in range(cantidad):
@@ -23,41 +23,45 @@ while opcion != 4:
             nombre = input("Nombre completo del estudiante: ")
             edad = int(input("Edad: "))
             carrera = input("Carrera: ")
-            cantidadCurso =int(input("Cantidad de curso que desea registrar: "))
-            while cantidad <= 0 or cantidad >= 50:
+            cantidadCurso = int(input("Cantidad de curso que desea registrar: "))
+            while cantidadCurso <= 0 or cantidadCurso >= 50:
                 print("Ha ingresado un numero fuera de los limites,vuelva a intentarlo")
                 cantidadCurso = int(input("Cantidad de curso que desea registrar: "))
             for j in range(cantidadCurso):
                 nombreCursoA = input("Nombre completo del curso: ")
-                nombreCurso = nombreCursoA
+                codigoCurso = input("Codigo del curso: ")
                 tarea = float(input("Nota de la Tarea del curso (0-100) : "))
                 parcial = float(input("Nota de el parcial del curso (0-100): "))
                 proyecto = float(input("Nota de la proyecto del curso (0-100): "))
+                promedio = (tarea + parcial + proyecto) / 3
                 estudiantes[carnet] = {
                     "nombre": nombre,
                     "edad": edad,
                     "carrera": carrera,
-                    "nombreCursoA": nombreCursoA,
-                    "nombreCurso": {
+                    "codigoCurso": {
+                        "nombreCursoA": nombreCursoA,
                         "tarea": tarea,
                         "parcial": parcial,
-                        "proyecto": proyecto
+                        "proyecto": proyecto,
+                        "promedio": promedio
                     }
 
                 }
 
     if opcion == 2:
         print("=== Mostrar lista de Estudiantes y cursos ===")
-        for carnet,datos in estudiantes.items():
+        for carnet, datos in estudiantes.items():
             print(f"\nCarnet: {carnet}")
             print(f"Nombre: {datos['nombre']}")
             print(f"Edad: {datos['edad']}")
             print(f"Carrera: {datos['carrera']}")
             print("Cursos inscritos:")
-            print(f"nombre del curso: {datos['nombreCursoA']}")
+            for cursos in datos["codigoCurso"]:
+                print(f"nombre del curso: {datos['nombreCursoA']}")
             print(f"Punteo tarea: {datos['nombreCurso']['tarea']}")
             print(f"Punteo parcial: {datos['nombreCurso']['parcial']}")
             print(f"Punteo proyecto: {datos['nombreCurso']['proyecto']}")
+            print(f"promedio: {datos['nombreCurso']['promedio']}")
 
     if opcion == 3:
         print("=== Buscar estudiante por carnet ===")
